8f60867746f89109ff82ed1149faa9e096f11569
sandra-mutai <sandramuttai@gmail.com>
2021-03-10 21:54:51 +0300

    sandra

---
+++ src/Sandra.kt
@@ -0,0 +1,15 @@
+fun main() {
+    var name="sandra"
+    name="sandra"
+    println(name)
+
+    var age=19
+    println(age)
+
+    var citizenship="true"
+    println(citizenship)
+
+    var weight=60.5467328
+    println(weight)
+
+    }
